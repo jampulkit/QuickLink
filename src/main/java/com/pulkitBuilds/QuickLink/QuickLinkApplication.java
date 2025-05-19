4f44e104f4dd82d68b674014889e0f1e96744e72
pulkit <jampulkit@gmail.com>
2025-05-19 07:20:28 +0530

    Initial project setup with Spring Boot and Git

---
+++ src/main/java/com/pulkitBuilds/QuickLink/QuickLinkApplication.java
@@ -0,0 +1,13 @@
+package com.pulkitBuilds.QuickLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulkitBuilds/QuickLink/QuickLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulkitBuilds.QuickLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
