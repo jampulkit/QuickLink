46dcff6d9500c926d88dfcdef461ca19a4fad98c
pulkit <jampulkit@gmail.com>
2025-05-19 07:46:26 +0530

    feat: define entities , repositories and H2 configuretion

---
+++ src/main/java/com/pulkitBuilds/QuickLink/Entity/UrlMapping.java
@@ -0,0 +1,69 @@
+package com.pulkitBuilds.QuickLink.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url_mapping")
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long  Id;
+    @Column(nullable = false,unique = true,length = 10)
+    private String shortCode;
+    @Lob
+    @Column(nullable = false,columnDefinition = "TEXT(255)")
+    private String longUrl;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public UrlMapping(String shortCode, String longUrl, LocalDateTime createdAt) {
+        this.shortCode = shortCode;
+        this.longUrl = longUrl;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "UrlMapping{" +
+                "Id=" + Id +
+                ", shortCode='" + shortCode + '\'' +
+                ", longUrl='" + longUrl + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/pulkitBuilds/QuickLink/Repo/Repository.java
@@ -0,0 +1,12 @@
+package com.pulkitBuilds.QuickLink.Repo;
+
+import com.pulkitBuilds.QuickLink.Entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<UrlMapping,Long> {
+    UrlMapping findByShortCode(String shortCode);
+    UrlMapping findByLongUrl(String longUrl);
+    boolean existsByShortCode(String shortCode);
+    boolean existsByLongUrl(String longUrl);
+
+}
